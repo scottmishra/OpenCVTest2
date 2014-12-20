a67419e22a102c02492e51f3fd0b6d5edfbfda03
Scott <Scott@Scott-PC>
2014-12-20 11:28:52 -0700

    Added a Image Selection to the app

--- src/com/example/test/MainActivity.java
+++ src/com/example/test/MainActivity.java
@@ -2,16 +2,23 @@ package com.example.test;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.database.Cursor;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.EditText;
 
 public class MainActivity extends Activity {
-	
+
 	public static final String EXTRA_MESSAGE = "com.example.test.MESSAGE";
+	public final int SELECT_PICTURE = 1;
 	
+	private String selectedImagePath;
+	private String filemanagerstring;
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -36,20 +43,71 @@ public class MainActivity extends Activity {
 		}
 		return super.onOptionsItemSelected(item);
 	}
-	
+
 	/**
 	 * Triggered when the find path button is clicked
+	 * 
 	 * @param view
 	 */
-	public void findPath(View view){
-		Intent intent = new Intent(this,DisplayMessageActivity.class );
-		//Get Text Field from the xml
-		EditText pathText = (EditText)findViewById(R.id.edit_message);
-		String messageText = pathText.getText().toString();
-		
-		//Put info into the intent that we want to start
-		intent.putExtra(EXTRA_MESSAGE, messageText);
-		//Creates the new activity based on the passed in intent obj.
-		startActivity(intent);
+	public void findPath(View view) {
+		// in onCreate or any event where your want the user to
+		// select a file
+		Intent intent = new Intent();
+		intent.setType("image/*");
+		intent.setAction(Intent.ACTION_GET_CONTENT);
+		startActivityForResult(Intent.createChooser(intent, "Select Picture"),
+				SELECT_PICTURE);
+	}
+
+	/**
+	 * Called from the startActivitForResult intent call.
+	 */
+	public void onActivityResult(int requestCode, int resultCode, Intent data) {
+		if (resultCode == RESULT_OK) {
+			if (requestCode == SELECT_PICTURE) {
+				Uri selectedImageUri = data.getData();
+
+				// OI FILE Manager
+				filemanagerstring = selectedImageUri.getPath();
+
+				// MEDIA GALLERY
+				selectedImagePath = getPath(selectedImageUri);
+
+				// DEBUG PURPOSE - you can delete this if you want
+				if (selectedImagePath != null)
+					System.out.println(selectedImagePath);
+				else
+					System.out.println("selectedImagePath is null");
+				if (filemanagerstring != null)
+					System.out.println(filemanagerstring);
+				else
+					System.out.println("filemanagerstring is null");
+
+				// NOW WE HAVE OUR WANTED STRING
+				if (selectedImagePath != null)
+					System.out
+							.println("selectedImagePath is the right one for you!");
+				else
+					System.out
+							.println("filemanagerstring is the right one for you!");
+			}
+		}
+	}
+
+	/*
+	 * Return the string of the path to the image
+	 */
+	public String getPath(Uri uri) {
+		String[] projection = { MediaStore.Images.Media.DATA };
+		Cursor cursor = managedQuery(uri, projection, null, null, null);
+		if (cursor != null) {
+			// HERE YOU WILL GET A NULLPOINTER IF CURSOR IS NULL
+			// THIS CAN BE, IF YOU USED OI FILE MANAGER FOR PICKING THE MEDIA
+			int column_index = cursor
+					.getColumnIndexOrThrow(MediaStore.Images.Media.DATA);
+			cursor.moveToFirst();
+			return cursor.getString(column_index);
+		} else
+			return null;
 	}
 }
